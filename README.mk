b1820236d9d5b13e58d4eeab8aef2e84d2360654
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-08-09 16:51:07 -0700

    this commit adds readme

---
+++ README.mk
@@ -0,0 +1,28 @@
+# _{Portfolio}_
+
+#### _{Webpage describing "me"}, {9 August 2019}_
+
+#### By _**{Alan Hermanns}**_
+
+## Description
+
+_{This is a webpage accessible via github which includes information about "me."}_
+
+## Setup/Installation Requirements
+
+* _Internet access_
+* _A web browser_
+
+## Support and contact details
+
+_{Please contact Alan via Github}_
+
+## Technologies Used
+
+_{CSS is used to style the page, git is used to record its progress, the bootstrap cdn has been employed, and the webpage is written in html.}_
+
+### License
+
+*{License MIT.}*
+
+Copyright (c) 2016 **_{Alan Hermanns}_**
